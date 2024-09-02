76aaa07acb744849f0450b2d7b8b18dccf1ef5a7
Suradis.S <suradis.workspace@gmail.com>
2024-09-02 18:26:14 +0700

    feat: add theme

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -2,7 +2,7 @@ return {
 
   { -- Linting
     'mfussenegger/nvim-lint',
-    event = { 'BufReadPre', 'BufNewFile' },
+    event = { 'BufReadPre', 'BufNewFile', 'InsertLeave' },
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
